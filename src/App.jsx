21d923361d973fbe637d9fcb67f87d974d56f1c5
Miguel Carhuas <mikel_ct@hotmail.com>
2024-08-14 09:17:11 -0500

    add content

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import Sidebar from './components/Sidebar'
+import Sidebar from './components/Sidebar';
+import Content from './components/Content';
+import Profile from './components/Profile';
+import './App.css';
 
 const App = () => {
   return (
